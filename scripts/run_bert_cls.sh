390580f27c5a06f8e43b822160ddc74922cdfe2b
hellen <144710795+hellen9527@users.noreply.github.com>
2024-04-07 15:28:19 +0800

    Update run_bert_cls.sh

--- scripts/run_bert_cls.sh
+++ scripts/run_bert_cls.sh
@@ -15,6 +15,7 @@ CUDA_VISIBLE_DEVICES=0 python -u runs/run_bert_cls.py \
     --output_dir "./outputs" \
     --num_train_epochs 3 \
     --learning_rate 1e-5 \
+    --data_format json \
     --per_gpu_train_batch_size 64 \
     --per_gpu_eval_batch_size 256 \
     --logging_steps 100
